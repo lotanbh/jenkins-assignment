d9bdf266c75b3b5806bc951fcdbae1a11ab98b2a
lotan <lotanbh@gmail.com>
2026-08-21 16:29:28 +0300

    testing beneath 70%

--- api/test/index.test.js
+++ api/test/index.test.js
@@ -4,11 +4,11 @@ const app = require('../src/app');
 describe('API Service Tests', () => {
 
     // בדיקה לנתיב המידע /api/data
-    it('should return data from /api/data', async () => {
-        const res = await request(app).get('/api/data');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('message');
-    });
+    // it('should return data from /api/data', async () => {
+    //     const res = await request(app).get('/api/data');
+    //     expect(res.statusCode).toEqual(200);
+    //     expect(res.body).toHaveProperty('message');
+    // });
 
     // בדיקה לנתיב הבריאות /health
     it('should return health status ok', async () => {
